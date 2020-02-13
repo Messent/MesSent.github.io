1e7e0bce378ca9f8229168e1bb8ce3db1b4f68c9
Messent <601992863@qq.com>
2020-02-13 21:21:39 +0800

    提交h5补差价页面

--- static/js/orderPay.js
+++ static/js/orderPay.js
@@ -19,9 +19,19 @@ const request = (url, data = {}, method = 'post') => {
 }
 let orderPay = {
     //订单支付信息
-    getExtraList(order_code) {
+    getOrderPremium(order_code) {
         return request(
-            'http://bs.paotui.com/v1/order/GetPayCode',
+            'http://bs.paotui.com/v1/order_premium/info',
+            {
+                order_code,
+            }
+        )
+    },
+
+    //刷新补差价二维码
+    refreshOrderPremium(order_code) {
+        return request(
+            'http://bs.paotui.com/v1/orderPremium/refresh',
             {
                 order_code,
             }
